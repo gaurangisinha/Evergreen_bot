c36b8e76e80a32e9f7aad60d5208317e15be9b3f
Sayantan-world <sayantan.world98@gmail.com>
2020-06-26 12:37:05 +0530

    Vesion 3 Range fix

--- bot.py
+++ bot.py
@@ -16,15 +16,47 @@ def get_Quote():
 
 
 ## Number of days you want to make commits
-print("\nWelcome to Github commit in the past !! XD\n")
-n = int(input("Enter number of days u want to go back: "))
+print("""
+-------------------------------------------------------------
+EVERGREEN BOT -- Welcome to Github commit in the past !! XD
+-------------------------------------------------------------
+
+Instructions :
+--------------
+1. Enter the day number you want to rewind to (start) [Eg..  n = 1 to go to previous day].
+2. Enter the number of days you want to perform this action (n).
+3. Set the max limit of commit per day(m - Random value taken between 0-m).
+
+Recommended Settings : 
+----------------------
+1. Set start to any number u want
+2. Set number of days (n = 30)
+3. Set max limit ( m = 10 )
+
+Dangers :
+---------
+1. If you set n > 180 directly, the script will work but it  may take around 2 days to update your contribution chart.
+2. Do not put m > 20 as it will take a long time to finish executing the script.
+
+
+Samples :
+---------
+1. With the Recommended settings the script will take 1 min time to execute and the commits will be reflected instantly.
+
+
+""")
+
+start = int(input("Enter the day number you want to rewind to (Eg : 5 for going 5 days back): "))
+if start == 0:
+	start = 1
+n = int(input("Enter the number of days you want to perform this action (Eg : 10 for going from 5th day, back to 15th day): "))	
 maxm = int(input("Enter limit of max commit each day: "))
 
 # Erase contents of the file
 file = open("bot.txt","w")
 file.close()
 
-for i in range(1,n + 1):
+for i in range(start,start + n + 1):
 	end = random.randint(0,maxm)
 	for j in range(0,end):
 		d = str(i) + ' day ago'
